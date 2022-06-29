70176d78ad1e5690acb578566f5a8e0114105b72
chencheng <1842146958@qq.com>
2022-06-29 15:10:30 +0800

    提交路径

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	r := gin.Default()
+	r.GET("/index", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"messge": "hello world",
+		})
+	})
+	r.Run()
 }
